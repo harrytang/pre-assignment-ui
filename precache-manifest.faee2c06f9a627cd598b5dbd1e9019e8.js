2052bd4d2602d17ef2b61071268f6e35303709f4
Harry Tang <harry@powerkernel.com>
2019-11-30 23:59:56 +0200

    Updates

---
+++ precache-manifest.faee2c06f9a627cd598b5dbd1e9019e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74b8f9452a31c5013a0bc392ee9eb6d",
+    "url": "/pre-assignment-ui/index.html"
+  },
+  {
+    "revision": "afa49aa7d7c93d7330b8",
+    "url": "/pre-assignment-ui/static/js/2.3ee74abb.chunk.js"
+  },
+  {
+    "revision": "22bb93dd5c4273584325",
+    "url": "/pre-assignment-ui/static/js/main.ba1ff77a.chunk.js"
+  },
+  {
+    "revision": "e9b9826b9aac5c7ee764",
+    "url": "/pre-assignment-ui/static/js/runtime-main.3fb3fc20.js"
+  }
+]);
